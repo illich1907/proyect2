bb07751013a918f7369ca8fad65d508ed35e8474
illich1907 <anastasiazatovkanuk@gmail.com>
2024-07-03 17:43:00 +0300

    Create Edit Maain

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
 int main() {
+    const int a = 100;  
+    char b[a];
+
+    std::cout << "Введіть рядок: ";
+    std::cin.getline(b, a);
+
+    int c = 0;
+    while (b[c] != '\0' && c < a) {
+        c++;
+    }
+
+    int d = a - c;
+
+    std::cout << "Загальна кількість елементів у масиві: " << a << std::endl;
+    std::cout << "Кількість зайнятих елементів: " << c << std::endl;
+    std::cout << "Кількість вільних елементів: " << d << std::endl;
 
 }
